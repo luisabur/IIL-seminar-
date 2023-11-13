bd0f25f54c7d02ca79c6e3d50a23b9fb05dc3bd1
luisabur <150684209+luisabur@users.noreply.github.com>
2023-11-13 15:34:33 +0100

    Create vignettes.R

---
+++ vignettes.R
@@ -0,0 +1,21 @@
+install.packages('AlgDesign')
+library(AlgDesign)
+
+install.packages('ConfDesign')
+install.packages('conf.design')
+library(conf.design)
+
+
+dat<-gen.factorial(c(2,3,2,2,2,2,2),varNames=c("Specificity","Level","Training", "Recency", "Class", "Gender", "Age"))
+b_nr<-optBlock(~.^2, dat,c(8,8,8,8,8,8,8,8,8),criterion="D", nR=100)
+eval.blockdesign(~.^2,b_nr$design,c(8,8,8,8,8,8,8,8,8))
+eval.blockdesign(~(.)^2,b_nr$design,rep(8,8),confounding=TRUE)
+bk<-data.matrix(b_nr$Blocks$B1)
+t(bk)%*%bk
+bk<-data.matrix(b_nr$design)
+t(bk)%*%bk
+
+
+
+
+
