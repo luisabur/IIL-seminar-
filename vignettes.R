fb392ad8110e06cd20f055674b3e0b18e4449cdd
luisabur <150684209+luisabur@users.noreply.github.com>
2023-11-24 11:10:00 +0100

    Update vignettes.R

--- vignettes.R
+++ vignettes.R
@@ -1,12 +1,24 @@
-install.packages('AlgDesign')
+#install.packages('AlgDesign')
 library(AlgDesign)
 
-install.packages('conf.design')
+#install.packages('conf.design')
 library(conf.design)
 
+#generate matrix 
 
-dat<-gen.factorial(c(2,3,2,2,2,2,2),varNames=c("Specificity","Level","Training", "Recency", "Class", "Gender", "Age"))
-b_nr<-optBlock(~.^2, dat,c(8,8,8,8,8,8,8,8,8),criterion="D", nR=100)
+#all levels 
+datl<-gen.factorial(c(2,3,2,2,2,2,2,2),varNames=c("Specificity","Level","Training","Recency","Mobility","Leadership","Class","Gender"))
+#without leadership
+datwl<-gen.factorial(c(2,3,2,2,2,2,2),varNames=c("Specificity","Level","Training","Recency","Mobility","Class","Gender"))
+#without class 
+datac<-gen.factorial(c(2,3,2,2,2,4,2),varNames=c("Specificity","Level","Training","Recency","Mobility","Class","Gender"))
+
+#2-way interactions 
+b_nr<-optBlock(~.^2, dat, rep(8,20),criterion="D", nR=100) 
+eval.blockdesign(~(.)^2,b_nr$design,rep(8,16))
+eval.blockdesign(~(.)^2,b_nr$design,rep(8,16), confounding=TRUE)
+
+#3-way interactions 
 eval.blockdesign(~.^2,b_nr$design,c(8,8,8,8,8,8,8,8,8))
 eval.blockdesign(~(.)^2,b_nr$design,rep(8,8),confounding=TRUE)
 bk<-data.matrix(b_nr$Blocks$B1)
